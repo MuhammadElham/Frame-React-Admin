c2ead0391fc3ab82d226efcdc80d71043b55e19f
Elham <elhamwaheed777@gmail.com>
2025-05-08 00:58:02 +0500

    Working in Add

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { assets } from "../assets/assets";
+import axios from "axios";
 
 const Add = () => {
   // State for Images
@@ -17,8 +18,30 @@ const Add = () => {
   const [bestseller, setBestseller] = useState(false);
   const [sizes, setSizes] = useState([]);
 
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    try {
+
+      const formData = new FormData();
+
+      formData.append("name", name);
+      formData.append("description", description);
+      formData.append("price", price);
+      formData.append("category", category);
+      formData.append("subCategory", subCategory);
+      formData.append("bestseller", bestseller);
+      formData.append("sizes", JSON.stringify(sizes));
+
+      image1 && formData.append("image1", image1);
+      image2 && formData.append("image2", image2);
+      image3 && formData.append("image3", image3);
+      image4 && formData.append("image4", image4);
+
+    } catch (error) {}
+  };
+
   return (
-    <form className="flex flex-col w-full items-start gap-3">
+    <form onSubmit={onSubmitHandler} className="flex flex-col w-full items-start gap-3">
       <div>
         <p className="mb-2">Upload Image</p>
         <div className="flex gap-2">
