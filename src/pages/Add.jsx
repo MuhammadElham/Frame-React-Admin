0c1c98ca8aedf7b296e36fe7dc05ac0daddade7c
Elham <elhamwaheed777@gmail.com>
2025-05-07 23:27:46 +0500

    Working in Add

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -7,7 +7,7 @@ const Add = () => {
   const [image2, setImage2] = useState(false);
   const [image3, setImage3] = useState(false);
   const [image4, setImage4] = useState(false);
-  
+
   // State for Fields
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
@@ -43,18 +43,18 @@ const Add = () => {
       {/*  */}
       <div className="w-full">
         <p className="mb-2">Product Name</p>
-        <input onChange={(e)=>setName(e.target.value)} value={name} className="w-full max-w-[500px] px-3 py-2" type="text" placeholder="Type here" required />
+        <input onChange={(e) => setName(e.target.value)} value={name} className="w-full max-w-[500px] px-3 py-2" type="text" placeholder="Type here" required />
       </div>
 
       <div className="w-full">
         <p className="mb-2">Product Description</p>
-        <textarea className="w-full max-w-[500px] px-3 py-2" type="text" placeholder="Write content here" required />
+        <textarea onChange={(e) => setDescription(e.target.value)} value={description} className="w-full max-w-[500px] px-3 py-2" type="text" placeholder="Write content here" required />
       </div>
       {/*  */}
       <div className="flex flex-col sm:flex-row gap-2 w-full sm:gap-8">
         <div>
           <p className="mb-2">Product Category</p>
-          <select className="w-full px-3 py-2">
+          <select onChange={(e) => setCategory(e.target.value)} className="w-full px-3 py-2">
             <option value="Frame">Frame</option>
             <option value="Ring Trays">Ring Tray</option>
             <option value="Welcome Board">Welcome Board</option>
@@ -66,7 +66,7 @@ const Add = () => {
 
         <div>
           <p className="mb-2">Sub Category</p>
-          <select className="w-full px-3 py-2">
+          <select onChange={(e) => setSubCategory(e.target.value)} className="w-full px-3 py-2">
             <option value="Frame">Frame</option>
             <option value="Ring Trays">Ring Tray</option>
             <option value="Welcome Board">Welcome Board</option>
@@ -78,14 +78,14 @@ const Add = () => {
 
         <div>
           <p className="mb-2">Product Price</p>
-          <input className="w-full px-3 py-2 sm:w-[120px]" type="number" placeholder="25" />
+          <input onChange={(e) => setPrice(e.target.value)} value={price} className="w-full px-3 py-2 sm:w-[120px]" type="number" placeholder="25" />
         </div>
       </div>
       {/*  */}
       <div>
         <p className="mb-2">Product Sizes</p>
         <div className="flex gap-3">
-          <div>
+          <div onClick={() => setSizes((prev) => (prev.includes('12" x 24"') ? prev.filter((item) => item !== '12" x 24"') : [...prev]))}>
             <p className="bg-slate-200 px-3 py-1 cursor-pointer">12" x 24"</p>
           </div>
           <div>
