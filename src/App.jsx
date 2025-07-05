9c66bf37121b1c33d3ccfb712ff49c2301737101
Elham <elhamwaheed777@gmail.com>
2025-07-05 12:32:23 +0500

    Chagne Logo in ToolBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "./components/Navbar.jsx";
 import SideBar from "./components/SideBar.jsx";
 import { Route, Routes } from "react-router-dom";
